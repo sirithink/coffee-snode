7358df7b210e45e7b5b7dcf948f7609a0e80ff00
ChunMengLu <qq596392912@gmail.com>
2013-02-22 14:14:00 +0800

    update clear
    update clear

--- clear.bat
+++ clear.bat
@@ -1,4 +1,5 @@
 echo Please wait a moment...
+cake build
 rm -rf ../temp
 cp -r ../coffee-snode ../temp
 cd ../temp
@@ -7,6 +8,7 @@ rm -rf .git
 rm -rf .idea
 rm .gitignore
 rm Cakefile
+rm .travis.yml
 rm build.bat
 rm clear.bat
 rm strat.bat
