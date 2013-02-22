5ae3f3a02c8955b42b2e4576bfdad5125a161bd8
Chunmeng <596392912@qq.com>
2013-02-23 00:28:01 +0800

    up clear

--- clear.bat
+++ clear.bat
@@ -1,16 +1,13 @@
 echo Please wait a moment...
 rm -rf ../temp
 cake build
-cp -r ../coffee-snode ../temp
-cd ../temp
-rm -rf snode_server
-rm -rf .git
-rm -rf .idea
-rm .gitignore
-rm Cakefile
-rm .travis.yml
-rm build.bat
-rm clear.bat
-rm strat.bat
-rm togitcafe.bat
-echo OK Now!
+mkdir ../temp
+echo mkdir ../temp
+cp -r ../coffee-snode/node_modules ../temp/node_modules
+cp -r ../coffee-snode/public ../temp/public
+cp -r ../coffee-snode/snode ../temp/snode
+cp -r ../coffee-snode/views ../temp/views
+cp ../coffee-snode/app.js ../temp/app.js
+cp ../coffee-snode/package.json ../temp/package.json
+cp ../coffee-snode/readme.md ../temp/readme.md
+echo OK Now,Please switch to temp!
